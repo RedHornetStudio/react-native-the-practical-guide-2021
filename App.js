a3992a8edc111a10b5b57023287fc3de248cc1ff
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-12-19 11:19:10 +0200

    modified:   App.js new file:   assets/fonts/ImperialScript-Regular.ttf modified:   package-lock.json modified:   package.json new file:   screens/CategoriesScreen.js new file:   screens/CategoryMealsScreen.js new file:   screens/FavoritesScreen.js new file:   screens/FiltersScreen.js new file:   screens/MealDetailScreen.js

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    'ImperialScript-Regular': './assets/fonts/ImperialScript-Regular.ttf',
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +25,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontFamily: 'ImperialScript-Regular',
   },
 });
